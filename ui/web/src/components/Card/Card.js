419028a33a51cbad6289a951e1c9856c06a1cc8a
Yukiiro-Nite <e.kazuma@gmail.com>
2019-08-18 21:27:40 -0400

    Added Card component

---
+++ ui/web/src/components/Card/Card.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import classnames from 'classnames'
+import './Card.css'
+
+class Card extends React.Component {
+  static defaultProps = {
+    dark: true
+  }
+  render() {
+    const {children, className, dark} = this.props;
+    const childrenLength = React.Children.count(children)
+    const childrenWithClasses = React.Children.map(children, (child, index) =>
+      React.isValidElement(child)
+        ? React.cloneElement(child, {
+          className: classnames(child.classname, {
+            'first': index === 0,
+            'last': index === (childrenLength - 1)
+          })
+        })
+        : child
+    )
+    return (
+      <section className={classnames("Card", {'dark': dark, 'light': !dark}, className)}>
+        {childrenWithClasses}
+      </section>
+    )
+  }
+}
+
+export {Card}
