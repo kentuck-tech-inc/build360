b5458f4a275426e279103e5a43a2acbe466cdad1
Unknown <raedwa01@gmail.com>
2019-08-27 22:42:45 -0400

    Added sequelize and got initial response in lambda working

--- blueprint/api.js
+++ blueprint/api.js
@@ -1,14 +1,92 @@
 'use strict'
 
-const Api = require('claudia-api-builder')
-const api = new Api()
+const Api = require('claudia-api-builder');
+const api = new Api();
+var mariadb = require('mariadb');
+var {Op, Sequelize } = require('sequelize');
 
 const blueprint = require('./handlers/blueprint')
 const healthCheck = require('./handlers/healthcheck')
 
-api.get('/', () => Date.now())
+api.get('/blueprints', () => {
+    return new Promise(
+        (resolve, reject) => {
+            loadDatabase()
+            .then((db) => {
+                resolve(getBlueprints(db));
+            })
+            .catch((ex) => {
+                reject( `Ok, something went seriously wrong...\n ${ex.message}`);
+            });
+    });
+});
 
-api.get('/builder', async () => { return await blueprint.GetBlueprints()})
-api.get('/heartbeat', () => {return healthCheck.healthcheck()})
+api.get('/blueprints/search', (request) => {
+    return new Promise(
+        (resolve, reject) => {
+            loadDatabase()
+            .then((db) => {
+                resolve(searchBlueprints(db, request));
+            })
+            .catch((ex) => {
+                reject( `Ok, something went seriously wrong...\n ${ex.message}`);
+            });
+        }
+    );
+});
+
+function loadDatabase() {
+    var {Op, Sequelize} = require('sequelize');
+    return new Promise(
+        (resolve, reject) => {
+            const db = {};
+            const sequelize = new Sequelize(
+                'blueprint',
+                process.env.DB_USER,
+                process.env.DB_PASS,
+                {
+                    host: process.env.DB_HOST,
+                    dialect: 'mariadb',
+                    port: process.env.DB_PORT,
+                    pool: {
+                        max: 5,
+                        min: 0,
+                        idle: 300
+                    }
+            });
+            sequelize
+                .authenticate()
+                .then(() => {
+                    console.log('Connection has been established successfully.');
+                    db.blueprints = require('./models/blueprints')(sequelize, Sequelize);
+                    db.sequelize = sequelize;
+                    db.Sequelize = Sequelize;
+                    db.op = Op;
+                    resolve(db);
+                })
+                .catch(err => {
+                    console.error('Unable to connect to the database:', err);
+                    reject(err);
+                });
+        });
+}
+
+function getBlueprints(db) {
+    return new Promise(
+        (resolve, reject) => {
+            db.blueprints.findAll().then((result) => {
+                db.sequelize.connectionManager.close();
+                resolve(result);
+            }).error((err) => {
+                console.log("Error:" + err);
+                reject(err);
+            });
+    });
+}
+
+// api.get('/', () => Date.now())
+
+// api.get('/builder', async () => { return await blueprint.GetBlueprints()})
+// api.get('/heartbeat', () => {return healthCheck.healthcheck()})
 
 module.exports = api
--- blueprint/express.js
+++ blueprint/express.js
@@ -1,16 +1,80 @@
 const express = require('express')
 require('dotenv').config()
+var mariadb = require('mariadb');
+var {Op, Sequelize } = require('sequelize');
+//const blueprint = require('./handlers/blueprint')
 
-const blueprint = require('./handlers/blueprint')
 const healthCheck = require('./handlers/healthcheck')
 
 const app = express()
 const port = 3000
 
-app.get('/builder', async (req, res) => {
-    let tmp = await blueprint.GetBlueprints();
-    res.status(200).send(tmp)
+// app.get('/builder', async (req, res) => {
+//     let tmp = await blueprint.GetBlueprints();
+//     res.status(200).send(tmp)
+// });
+app.get('/blueprints', () => {
+    return new Promise(
+        (resolve, reject) => {
+            loadDatabase()
+            .then((db) => {
+                resolve(getBlueprints(db));
+            })
+            .catch((ex) => {
+                reject( `Ok, something went seriously wrong...\n ${ex.message}`);
+            });
+    });
 });
+
 app.get('/health', (req, res) => res.status(200).send(healthCheck.healthcheck()))
 
 app.listen(port, () => console.log(`Blueprint app listening on port ${port}`))
+
+function loadDatabase() {
+    var {Op, Sequelize} = require('sequelize');
+    return new Promise(
+        (resolve, reject) => {
+            const db = {};
+            const sequelize = new Sequelize(
+                'blueprint',
+                process.env.DB_USER,
+                process.env.DB_PASS,
+                {
+                    host: process.env.DB_HOST,
+                    dialect: 'mariadb',
+                    port: process.env.DB_PORT,
+                    pool: {
+                        max: 5,
+                        min: 0,
+                        idle: 300
+                    }
+            });
+            sequelize
+                .authenticate()
+                .then(() => {
+                    console.log('Connection has been established successfully.');
+                    db.blueprints = require('./models/blueprints')(sequelize, Sequelize);
+                    db.sequelize = sequelize;
+                    db.Sequelize = Sequelize;
+                    db.op = Op;
+                    resolve(db);
+                })
+                .catch(err => {
+                    console.error('Unable to connect to the database:', err);
+                    reject(err);
+                });
+        });
+}
+
+function getBlueprints(db) {
+    return new Promise(
+        (resolve, reject) => {
+            db.blueprints.findAll().then((result) => {
+                db.sequelize.connectionManager.close();
+                resolve(result);
+            }).error((err) => {
+                console.log("Error:" + err);
+                reject(err);
+            });
+    });
+}
