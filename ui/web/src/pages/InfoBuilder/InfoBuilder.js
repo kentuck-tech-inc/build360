ac3b138855cd5c49a9fb2aa00b088239b42cae07
Nicholaus Lawson <ccrngd1@gmail.com>
2020-02-15 13:22:34 -0500

    finish users page

--- ui/web/src/pages/InfoBuilder/InfoBuilder.js
+++ ui/web/src/pages/InfoBuilder/InfoBuilder.js
@@ -3,11 +3,11 @@ import { Card } from '../../components/Card/Card'
 import { FadeInOnVisible } from '../../components/FadeInOnVisible/FadeInOnVisible'
 import { Image } from '../../components/Image/Image'
 import { Link } from '../../components/Link/Link'
+import Carousel from 'react-bootstrap/Carousel'
 
-import newHome from '../../assets/new-home.jpg'
-import tech from '../../assets/technology.jpg'
-import calc from '../../assets/calculator.jpg' 
-import Carousel from 'react-bootstrap/Carousel';
+import businessman from '../../assets/businessman.jpg'
+import meeting from '../../assets/meeting.jpg'
+import stop from '../../assets/stop.jpg' 
 
 import './InfoBuilder.css'
 
@@ -15,20 +15,44 @@ class InfoBuilderPage extends React.Component {
   render () {
     return (
       <section className="InfoBuilder">        
-        <div className="top-content">
+        <div className="top-content">  
         <Carousel>
-          <Carousel.Item>
-            <Image className="d-block w-100" alt="newly built home" src={newHome} /> 
-          </Carousel.Item>
-          <Carousel.Item>
-            
-						<img className="d-block w-100" alt="blueprint stock image" src={tech}/> 
-          </Carousel.Item>
-          <Carousel.Item>
-						<img className="d-block w-100" alt="calculator stock image" src={calc} /> 
-          </Carousel.Item>
-        </Carousel>
-        
+            <Carousel.Item>
+              <Image className="d-block w-100" alt="newly built home" src={stop} /> 
+              <Carousel.Caption>
+                  <h2 className="card-title">
+                    Save Time.
+                  </h2>
+                  <p className="card-text">
+                    No more unnecessary communication between you and your next client - just to have them sign with a competitor. Set clear expectations instantly. Now you can focus on what matters most. Maximum efficiency is here.
+                  </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>            
+              <img className="d-block w-100" alt="blueprint stock image" src={meeting}/> 
+              <Carousel.Caption> 
+                  <h2 className="card-title">
+                    Deliver Accurate Estimates.
+                  </h2>
+                  <p className="card-text">
+                    Your next client already knows what home they want to build. From floor to ceiling, finishes are already known in advance. Finally, a solution that eliminates the dreaded "allowances". Stand out from the rest now.
+                  </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className="d-block w-100" alt="calculator stock image" src={businessman} /> 
+              <Carousel.Caption>
+                <h2 className="card-title">
+                  Generate More Revenue.
+                </h2>
+                <p>
+                  Showcase your work with your detailed builder profile, receiving new clients with their home and its finishes already selected. Eliminate wasted time and gain your client's trust with their best possible first impression.
+                </p>
+              </Carousel.Caption>
+            </Carousel.Item>
+          </Carousel> 
+        </div> 
+
         <Card className="flex flex-col items-center">
             <p className="text-center lead">
               Build360 allows prospective clients the opportunity to submit their new home plan with all the finishes already selected, saving you time and money. Your next client is already here.
@@ -42,34 +66,6 @@ class InfoBuilderPage extends React.Component {
               Estimating, interviewing and sourcing...all in one place.<br />              
             </p>
             </Card>
-        </div>
-
-        <FadeInOnVisible className="next-section grid-column-3">
-          <Card className="flex flex-col items-center">
-							<h2 className="card-title">
-								Save Time.
-							</h2>
-							<p className="card-text">
-								No more unnecessary communication between you and your next client - just to have them sign with a competitor. Set clear expectations instantly. Now you can focus on what matters most. Maximum efficiency is here.
-							</p>
-          </Card>
-          <Card className="flex flex-col items-center">
-							<h2 className="card-title">
-								Deliver Accurate Estimates.
-							</h2>
-							<p className="card-text">
-								Your next client already knows what home they want to build. From floor to ceiling, finishes are already known in advance. Finally, a solution that eliminates the dreaded "allowances". Stand out from the rest now.
-							</p>
-          </Card>
-          <Card className="flex flex-col items-center">
-							<h2 className="card-title">
-								Generate More Revenue.
-							</h2>
-							<div>
-								Showcase your work with your detailed builder profile, receiving new clients with their home and its finishes already selected. Eliminate wasted time and gain your client's trust with their best possible first impression.
-							</div>
-          </Card>
-        </FadeInOnVisible>
 
         <FadeInOnVisible className="next-section grid-column-2">
           
--- ui/web/src/pages/InfoUser/InfoUser.js
+++ ui/web/src/pages/InfoUser/InfoUser.js
@@ -3,112 +3,80 @@ import { Card } from '../../components/Card/Card'
 import { FadeInOnVisible } from '../../components/FadeInOnVisible/FadeInOnVisible'
 import { Image } from '../../components/Image/Image'
 import { Link } from '../../components/Link/Link'
+import Carousel from 'react-bootstrap/Carousel';
+import Jumbotron from 'react-bootstrap/Jumbotron'
+import { RandomImage } from '../../components/RandomImage/RandomImage'
+import { Tabs } from '../../components/Tabs/Tabs'
 
+import newHome from '../../assets/new-home.jpg'
+import house1 from '../../assets/houses/modern/house-1.jpg'
 import house2 from '../../assets/houses/contemporary/house-2.jpg'
-import logo from '../../assets/build360-logo.svg'
+import builder1 from '../../assets/builders/builder-1.jpg'
+import floorplan from '../../assets/default-floorplan.jpg'
+import receiveQuotes from '../../assets/receive-quotes.jpg'
 import builderSVG from '../../assets/builder.svg'
 import blueprintSVG from '../../assets/blueprint.svg'
-import Carousel from 'react-bootstrap/Carousel';
+import moneySVG from '../../assets/money.svg'
+import logo from '../../assets/build360-logo.svg'
+import feedback from '../../assets/feedback.jpg'
 
 import './InfoUser.css' 
 
 class InfoUserPage extends React.Component {
   render () {
     return (
-      <section className="InfoUser">        
+      <section className="InfoUser"> 
         <div className="top-content">
-        <Carousel>
-          <Carousel.Item>
-            <Image className="d-block w-100" alt="Carousel Bootstrap First" src="https://www.layoutit.com/img/sports-q-c-1600-500-1.jpg" />
-            <Carousel.Caption>
-              <h4>
-								First Thumbnail label
-							</h4>
-							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-							</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-            
-						<img className="d-block w-100" alt="Carousel Bootstrap Second" src="https://www.layoutit.com/img/sports-q-c-1600-500-2.jpg" />
-
-            <Carousel.Caption>
-							<h4>
-								Second Thumbnail label
-							</h4>
-							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-							</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-          <Carousel.Item>
-						<img className="d-block w-100" alt="Carousel Bootstrap Third" src="https://www.layoutit.com/img/sports-q-c-1600-500-3.jpg" />
-
-            <Carousel.Caption>
-							<h4>
-								Third Thumbnail label
-							</h4>
-							<p>
-								Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.
-							</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        </Carousel>
-        
-        <Card className="flex flex-col items-center">
-            <p className="text-center lead">
-              Build360 allows prospective clients the opportunity to submit their new home plan with all the finishes already selected, saving you time and money. Your next client is already here.
-            </p>
-            
-            <p>
-              No more leads. No more advertising. No more wasted time.
-            </p>
-
-            <p>
-              Estimating, interviewing and sourcing...all in one place.<br />              
-            </p>
-            </Card>
+          <img className="HomePage-img" src={house2} />
+          <Card className="hover-content">
+            <h2>
+              The home of your dreams is waiting for you.
+            </h2>
+            <h2>
+              <span>Find your favorite builder and house plan. Customize your home and receive quotes from area builders.</span>
+            </h2>
+          </Card>
         </div>
 
-        <FadeInOnVisible className="next-section grid-column-3">
-          <Card className="flex flex-col items-center">
-							<h2 className="card-title">
-								Save Time.
-							</h2>
-							<p className="card-text">
-								No more unnecessary communication between you and your next client - just to have them sign with a competitor. Set clear expectations instantly. Now you can focus on what matters most. Maximum efficiency is here.
-							</p>
-          </Card>
-          <Card className="flex flex-col items-center">
-							<h2 className="card-title">
-								Deliver Accurate Estimates.
-							</h2>
-							<p className="card-text">
-								Your next client already knows what home they want to build. From floor to ceiling, finishes are already known in advance. Finally, a solution that eliminates the dreaded "allowances". Stand out from the rest now.
-							</p>
-          </Card>
-          <Card className="flex flex-col items-center">
-							<h2 className="card-title">
-								Generate More Revenue.
-							</h2>
-							<div>
-								Showcase your work with your detailed builder profile, receiving new clients with their home and its finishes already selected. Eliminate wasted time and gain your client's trust with their best possible first impression.
-							</div>
-          </Card>
-        </FadeInOnVisible>
-
         <FadeInOnVisible className="next-section grid-column-2">
-          
-          <Card className="flex flex-col items-center">
-            
-          </Card>
-          <Card className="flex flex-col items-center">
-            <h2>Build360 combines the best of estimating and leads all in one easy to use solution.</h2>
-            <p className="mt-4">
-            Ryan Berry with Andy's Custom Homes of Kentucky
-            </p>
-          </Card>
+          <section>
+            <h2 className="offset-header" id="browse-builders">Browse Builders</h2>
+            <p className="mt-4">View all home builders that currently build homes in your neighborhoods. Search by name, business name, zip code and filter by BBB Rating, Build360 Score, Year Established and more! Every builder showcases their work on their profile, full of client feedback, photos of their jobs, a short biography of them and house plans they have built.</p>
+            <Link className="block mt-4" to="/builders">Search for builders</Link>
+          </section>
+          <Image src={builder1} alt="builder image" />
+        </FadeInOnVisible>
+        <FadeInOnVisible className="next-section grid-column-2">
+          <Image className="self-center" src={floorplan} alt="floorplan image" />
+          <section>
+            <h2 className="offset-header" id="view-plans">View Plans</h2>
+            <p className="mt-4">Search thousands of house plans for free. Found a favorite, but not "just right"? Customize that plan in virtual reality to get the exact features you want! Taller ceilings, larger master bath, bigger garage, the list goes on.</p>
+            <div className="mt-4">
+              <Tabs
+                tabs={[
+                  'Contemporary',
+                  'Traditional',
+                  'Modern'
+                ]}
+                content={
+                  ['contemporary', 'traditional', 'modern'].map((type, groupIndex) => (
+                    <div key={groupIndex} className="grid-column-2 grid-gap-4">
+                      {
+                        new Array(4).fill().map((_, index) => <RandomImage index={index} key={`${groupIndex}-${index}`} from={type}/>)
+                      }
+                    </div>
+                  ))
+                }
+              />
+            </div> 
+          </section>
+        </FadeInOnVisible>
+        <FadeInOnVisible className="next-section grid-column-2">
+          <section>
+            <h2 className="offset-header" id="receive-quotes">Recieve quotes</h2>
+            <p className="mt-4">Once you select the plan you like, receive quotes from all the builders you're impressed with. Along with the quote, an anticipated start date so you can see how your builder's schedule is looking. If your quote comes back high, easily go back to that plan to make changes to get that quote in your budget.</p>
+          </section>
+          <Image src={receiveQuotes} alt="Quote image" />
         </FadeInOnVisible>
       </section>
     )
